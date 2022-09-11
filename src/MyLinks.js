5c976f15dbe95796e5e2b837def5ae1432826b82
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-09-11 00:41:04 -0400

    fixed

--- src/MyLinks.js
+++ src/MyLinks.js
@@ -8,6 +8,8 @@ function Contact() {
                 <p>GitHub: <a target="_blank" href="https://github.com/RyanWoodruff2002" >https://github.com/RyanWoodruff2002</a></p>
                 <p>My blog: <a target="_blank" href="https://medium.com/@ryanwoodruff2002" >https://medium.com/@ryanwoodruff2002</a></p>
                 <p>My resume: <a target="_blank" href="https://docs.google.com/document/d/17KnkWZXcEuDx6dSomRPp4Cu37WaO0CgU7bF6WHBXX4k/edit?usp=sharing" >https://docs.google.com/document/d/17KnkWZXcEuDx6dSomRPp4Cu37WaO0CgU7bF6WHBXX4k/edit?usp=sharing</a></p>
+                <p>Email: ryanwoodruff2002@gmail.com</p>
+                <p>Phone Number: (814) 596-2385</p>
             </ul>
         </div>
     )
