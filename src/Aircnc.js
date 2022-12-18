f85338b0922ee58e9e4013e0b6d48c4f0f0c61ba
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-12-18 01:37:15 -0500

    added projects

--- src/Aircnc.js
+++ src/Aircnc.js
@@ -3,8 +3,28 @@ import React from "react";
 function Aircnc() {
 
     return (
-        <div>
-            Aircnc
+        <div className="grid justify-items-center items-center">
+            <br/>
+            <h1 className="text-5xl text-white centered pt-5 pb-5 bg-[#1F7A8C] w-screen">Aircnc</h1>
+            <br/>
+            <p className="tracking-wide w-2/3 mb-3 text-xl text-black-500 md:text-xl dark:text-black-400">
+                Aircnc is the fourth group project I worked on at Flatiron. This project was a collaboration between me and one fellow student. This was unusual as groups of three were assigned for previous projects. Our primary goal was to make a full-stack project using our newly learned Ruby on Rails skills with full CRUD and user authentication. User auth was especially difficult to implement during this project as we were given less than a week to learn it.
+            </p>
+            <p className="tracking-wide w-2/3 mb-3 text-xl text-black-500 md:text-xl dark:text-black-400">
+                The idea behind Aircnc was to make a clone website of airbnb. Given the time constraints and difficulty of learning user auth while implementing it, many features and functionality were not get developed. Upon launching the app you are presented with a splash pages prompting the user to sign up or log in. Both work as intended and when successfull render the listings page. The listings page renders several cards, one for each listing instance in the database. A listing card has a reviews button to dynamically render the reviews associated with that listing. While toggled to be visible, reviews can be read, edited, and deleted. A form is also rendered prompting the user to input a rating and the text content of their review. When the user is finished they may logout using the button in the nav bar.
+            </p>
+            <br/>
+            <a target="_blank" className="text-purple-700 text-xl no-underline hover:underline ..."  href="https://github.com/Millsy24/phase-4-project" >GitHub Repo</a>
+            <br/>
+            <iframe
+                width="800"
+                height="400"
+                src="https://www.youtube.com/embed/y56mV6r59d8">
+            </iframe>
+            <br/>
+            <a target="_blank" className="text-purple-700 text-xl no-underline hover:underline ..." href="https://www.youtube.com/watch?v=y56mV6r59d8" >Video Demo</a>
+            <br/>
+            <br/>
         </div>
     )
 }
--- src/TicketRuler.js
+++ src/TicketRuler.js
@@ -3,8 +3,31 @@ import React from "react";
 function TicketRuler() {
 
     return (
-        <div>
-            TicketRuler
+        <div className="grid justify-items-center items-center">
+            <br/>
+            <h1 className="text-5xl text-white centered pt-5 pb-5 bg-[#1F7A8C] w-screen">Ticket Ruler</h1>
+            <br/>
+            <p className="tracking-wide w-2/3 mb-3 text-xl text-black-500 md:text-xl dark:text-black-400">
+                Ticket Ruler is the my third group project at Flatiron. Our objective was to make a basic front-end that would interact with our Ruby back-end. We created a website inspired by <a target="_blank" href="https://www.ticketmaster.com/" >ticketmaster</a>. The goal was to implement full CRUD and get the front-end and back-end communicating using Rake.
+            </p>
+            <p className="tracking-wide w-2/3 mb-3 text-xl text-black-500 md:text-xl dark:text-black-400">
+                Initial render will load the homepage. The homepage has a search bar to filter the show cards by show name, artist, or show time. Show cards have a Purchase Tickets button that will render a form prompting users to input party size. This will then create a ticket associated to the hard-coded initial user. The Account route initialy renders a create account form that will create a new user instance. The login button will render a different form prompting the user to input and email and password to login. This login is flawed but will change the hard-coded user to the inputed one if the email is found in the database. The My Tickets route will render ticket cards associated with the current user instance. Ticket cards have a edit party size button that renders a form to change party size on the ticket. Ticket cards also have a cancel button to delete the ticket instance. 
+            </p>
+            <p className="tracking-wide w-2/3 mb-3 text-xl text-black-500 md:text-xl dark:text-black-400">
+                The Ruby database has show, ticket, and user models. The user and show models have a many-to-many relationship joined by the ticket model.
+            </p>
+            <br/>
+            <a target="_blank" className="text-purple-700 text-xl no-underline hover:underline ..." href="https://github.com/wayland-i/ticketruler-project" >GitHub Repo</a>
+            <br/>
+            <iframe
+                width="800"
+                height="400"
+                src="https://www.youtube.com/embed/yqgnQ6X36dQ">
+            </iframe>
+            <br/>
+            <a target="_blank" className="text-purple-700 text-xl no-underline hover:underline ..." href="https://www.youtube.com/watch?v=yqgnQ6X36dQ" >Video Demo</a>
+            <br/>
+            <br/>
         </div>
     )
 }
